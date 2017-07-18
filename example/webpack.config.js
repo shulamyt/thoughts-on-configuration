a5387d35d8aad0fe23619df44e8613c77cec6ed4
Shulamyt <shulamyt@gmail.com>
2017-07-18 17:35:12 +0300

    gitignore

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,9 +1,13 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
 	entry: './src/index.js',
 	output: {
 	filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist')
-	}
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin()
+	]
 };
